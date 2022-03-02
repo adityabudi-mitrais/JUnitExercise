e89d294500f5484a6fcfccb3752edf1a05d6291f
Aditya Budi Laksono <adityabudi.laksono@mitrais.com>
2022-03-02 13:40:31 +0700

    update junit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,6 @@
 import model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,15 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        Main.users = Main.initiateUsers();
+    }
+
     @Test
     void initiateUsers() {
-        Main.users = Main.initiateUsers();
         assertEquals(2, Main.users.size());
     }
 
     @Test
     void sendPoint() {
-        Main.users = Main.initiateUsers();
         Main.sendPoint(1, 2, 10);
         assertEquals(40, Main.users.get(0).getPoint());
         assertEquals(60, Main.users.get(1).getPoint());
@@ -24,10 +29,9 @@ class MainTest {
 
     @Test
     void transactionHistories() {
-        Main.users = Main.initiateUsers();
-        Main.sendPoint(1, 2, 10);
-        assertEquals(1,Main.transactionHistories.size());
-        assertEquals("Adit",Main.transactionHistories.get(0).getSender());
-        assertEquals("Augi",Main.transactionHistories.get(0).getReceiver());
+        Main.sendPoint(2, 1, 20);
+        assertEquals(2,Main.transactionHistories.size());
+        assertEquals("Augi",Main.transactionHistories.get(1).getSender());
+        assertEquals("Adit",Main.transactionHistories.get(1).getReceiver());
     }
 }
